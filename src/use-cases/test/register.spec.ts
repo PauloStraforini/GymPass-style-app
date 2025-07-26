ebed5888a71f36c1a801e3c2f7256decbb3d811e
PauloStraforini <paulostraforini@gmail.com>
2025-07-26 10:31:40 -0300

    refactor: improve test structure and update password hashing test cases

--- src/use-cases/test/register.spec.ts
+++ src/use-cases/test/register.spec.ts
@@ -1,24 +1,38 @@
-import { describe, expect, it } from 'vitest'
-import { RegisterUseCase } from './register'
-import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
 import { compare } from 'bcryptjs'
+import { expect, describe, it } from 'vitest'
+
+import { RegisterUseCase } from './register'
 
 describe('Register Use Case', () => {
-  it('should hash user password', async () => {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          createdAt: new Date(),
+        }
+      },
+    })
 
     const { user } = await registerUseCase.execute({
       name: 'John Doe',
-      email: 'johdddn.doe@example.com',
-      password: 'pasdddsword123',
+      email: 'jndoe@example.com',
+      password: '123456',
     })
 
-    const isPasswordCorrectHashed = await compare(
-      'pasdddsword123',
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
       user.password_hash,
     )
 
-    expect(isPasswordCorrectHashed).toBe(true)
+    expect(isPasswordCorrectlyHashed).toBe(true)
   })
 })
