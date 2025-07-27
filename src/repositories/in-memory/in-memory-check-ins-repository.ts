9628244d3234e6591af0de337eefd78037813ced
PauloStraforini <paulostraforini@gmail.com>
2025-07-27 20:03:03 -0300

    feat: implement CheckInsRepository and InMemoryCheckInsRepository for managing check-ins

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,21 @@
+import { CheckInsRepository } from '../../repositories/check-ins-repository'
+import { Prisma, CheckIn } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public items: CheckIn[] = []
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validatedAt: data.validatedAt ? new Date(data.validatedAt) : null,
+      createdAt: new Date(),
+    }
+
+    this.items.push(checkIn)
+
+    return checkIn
+  }
+}
