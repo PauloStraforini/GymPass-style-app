17a87f95f2a855e4582e23032af370cb1faa063c
PauloStraforini <paulostraforini@gmail.com>
2025-07-29 00:42:07 -0300

    feat: implement findByIdOnDate method in CheckInsRepository and update CheckInUseCase to prevent multiple check-ins on the same day

--- src/use-cases/test/check-in.ts
+++ src/use-cases/test/check-in.ts
@@ -17,6 +17,15 @@ export class CheckInUseCase {
     userId,
     gymId,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkInOnSameDate = await this.checkInsRepository.findByIdOnDate(
+      userId,
+      new Date(),
+    )
+
+    if (checkInOnSameDate) {
+      throw new Error('You can only check in once per day.')
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
