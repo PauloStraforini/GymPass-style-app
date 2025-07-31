c6fc30197ca139397c26a37cdeae46af5ef3efad
PauloStraforini <paulostraforini@gmail.com>
2025-07-31 10:43:22 -0300

    feat: implement findManyNearby method in InMemoryGymsRepository for nearby gym retrieval

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,10 @@
+import {
+  findManyNearbyParams,
+  GymsRepository,
+} from '../../repositories/gyms-repository'
+import { getDistanceBetweenCoordinates } from '../../utils/get-distance-between-coordinates'
 import { Gym, Prisma } from '@prisma/client'
-import { GymsRepository } from '../gyms-repository'
-import { randomUUID } from 'crypto'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
@@ -15,7 +19,21 @@ export class InMemoryGymsRepository implements GymsRepository {
     return gym
   }
 
-  async searchMany(query: string, page: number): Promise<Gym[]> {
+  async findManyNearby(params: findManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
+  }
+
+  async searchMany(query: string, page: number) {
     return this.items
       .filter((item) => item.title.includes(query))
       .slice((page - 1) * 20, page * 20)
@@ -29,10 +47,9 @@ export class InMemoryGymsRepository implements GymsRepository {
       phoneNumber: data.phoneNumber ?? null,
       latitude: new Prisma.Decimal(data.latitude.toString()),
       longitude: new Prisma.Decimal(data.longitude.toString()),
-      createdAt: new Date(),
     }
 
-    this.items.push(gym)
+    this.items.push(gym as Gym)
 
     return gym
   }
