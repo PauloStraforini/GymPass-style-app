d3766880a04da992a7cf7fc366b62e7bd6ffd78e
PauloStraforini <paulostraforini@gmail.com>
2025-08-16 18:29:56 -0300

    feat: added new controllers for gyms, search and nearby

---
+++ src/http/controllers/gyms/search-gyms.ts
@@ -0,0 +1,21 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '../../../use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymBodySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymBodySchema.parse(request.body)
+
+  const searchGymUseCase = makeSearchGymsUseCase()
+
+  await searchGymUseCase.execute({
+    page,
+    query,
+  })
+
+  return reply.status(201).send()
+}
