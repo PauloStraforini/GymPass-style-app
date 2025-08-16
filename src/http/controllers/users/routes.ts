c02ac154503ae633921a1d74149c93022ae0d176
PauloStraforini <paulostraforini@gmail.com>
2025-08-16 17:38:39 -0300

    refactor: updated routes management

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,11 @@
+import { profile } from './profile'
+import { authenticate } from './authenticate'
+import { register } from './register'
+import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '../../middlewares/verify-jwt'
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+  app.post('/sessions', authenticate)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
