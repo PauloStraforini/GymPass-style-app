e63a428804f6a3ca963c46833d579d6df79a8cac
PauloStraforini <paulostraforini@gmail.com>
2025-08-17 18:10:29 -0300

    refactor: small changes in check-ins controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { userRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
+import { checkInRoutes } from './http/controllers/check-ins/routes'
 
 import { ZodError } from 'zod'
 import { env } from './env'
@@ -14,6 +15,7 @@ app.register(fastifyJwt, {
 
 app.register(userRoutes)
 app.register(gymsRoutes)
+app.register(checkInRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
